965bbe249ba4d1e490b1995e180136c05dcdaa1a
0xmostima <mostimasblunderbuss@protonmail.com>
2022-03-17 02:23:20 -0400

    Brotchain & no reshard EBMP

---
+++ src/EBMPnoReshard.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.11;
+
+import {Base64} from "./Base64.sol";
+
+contract EBMPnoReshard {
+    function uint32ToLittleEndian(uint32 a) internal pure returns (uint32) {
+        unchecked {
+            uint32 b1 = (a >> 24) & 255;
+            uint32 b2 = (a >> 16) & 255;
+            uint32 b3 = (a >> 8) & 255;
+            uint32 b4 = a & 255;
+            return uint32(b1 | (b2 << 8) | (b3 << 16) | (b4 << 24));
+        }
+    }
+
+    function encode(
+        bytes calldata data,
+        uint32 width,
+        uint32 height,
+        uint32 channels
+    ) public pure returns (string memory) {
+        bytes memory BITMAPFILEHEADER =
+            abi.encodePacked(
+                string("BM"),
+                uint32(
+                    uint32ToLittleEndian(14 + 40 + width * height * channels)
+                ), // the size of the BMP file in bytes
+                uint16(0), // Reserved 
+                uint16(0), // Reserved
+                uint32(uint32ToLittleEndian(14 + 40)) 
+                // the offset, i.e. starting address, of the byte where the bitmap 
+                // image data (pixel array) can be found
+            ); // total 2 + 4 + 2 + 2 + 4 = 14 bytes long
+        bytes memory BITMAPINFO =
+            abi.encodePacked(
+                uint32(0x28000000), // the size of this header, in bytes (40)
+                uint32(uint32ToLittleEndian(width)), // the bitmap width in pixels (signed integer)
+                uint32(uint32ToLittleEndian(height)), // the bitmap height in pixels (signed integer)
+                uint16(0x0100), // the number of color planes (must be 1)
+                uint16(0x1800), // the number of bits per pixel
+                uint32(0x00000000), // the compression method being used
+                uint32(uint32ToLittleEndian(width * height * channels)), // the image size
+                uint32(0xc30e0000), // the horizontal resolution of the image
+                uint32(0xc30e0000), // the vertical resolution of the image
+                uint32(0), // the number of colors in the color palette, or 0 to default to 2n
+                uint32(0) // the number of important colors used, or 0 when every color is important
+            ); // total 40 bytes long
+        string memory encoded =
+            Base64.encode(
+                abi.encodePacked(
+                    BITMAPFILEHEADER,
+                    BITMAPINFO,
+                    data
+                )
+            );
+        return encoded;
+    }
+}
--- src/test/benchmark.sol
+++ src/test/benchmark.sol
@@ -3,16 +3,24 @@ pragma solidity 0.8.11;
 
 import "ds-test/test.sol";
 import "src/EBMP.sol";
+import "src/EBMPnoReshard.sol";
 import "src/PixelationsRenderer.sol";
+import "src/Brotchain.sol";
 
 contract ContractTest is DSTest {
     uint8[] img;
+    bytes r_img;
+    bytes er_img;
     bytes img_bytes;
     
     uint randNonce = 0;
 
     PixelationsRenderer p_renderer;
+    Brotchain b_renderer;
     EBMP e_renderer;
+    EBMPnoReshard er_renderer;
+
+    bytes palette;
 
     function calc_mean(uint[] memory input) internal pure returns (uint) {
         uint s = 0;
@@ -67,15 +75,28 @@ contract ContractTest is DSTest {
         for (uint256 i = 0; i < 736; i++) {
             img_bytes[i] = getRandomByte();
         }
+
+        for (uint256 i = 0; i < 1024; i++) {
+            r_img[i] = getRandomByte();
+        }
+
+        for (uint256 i = 0; i < 3072; i++) {
+            er_img[i] = getRandomByte();
+        }
     }
 
     function setUp() public {
         img = new uint8[](3072);
+        r_img = new bytes(1024);
+        er_img = new bytes(3072);
         updateImage();
         img_bytes = new bytes(736);
         updateImageByte();
         p_renderer = new PixelationsRenderer();
         e_renderer = new EBMP();
+        b_renderer = new Brotchain();
+        er_renderer = new EBMPnoReshard();
+        palette = b_renderer.grayscale();
     }
 
     function testEBMP() public {
@@ -89,6 +110,32 @@ contract ContractTest is DSTest {
 
         emit log_uint(calc_mean(ebmp_length));
         emit log_uint(calc_stddev(ebmp_length));
+    }
+
+    function testEBMBnoReshard() public {
+
+        uint[] memory ebmp_length = new uint[](100);
+        for (uint i = 0; i < 100; i++) {
+            updateImageByte();
+            string memory ebmp = er_renderer.encode(er_img, 32, 32, 3);
+            ebmp_length[i] = bytes(ebmp).length;
+        }
+
+        emit log_uint(calc_mean(ebmp_length));
+        emit log_uint(calc_stddev(ebmp_length));
+    }
+
+    function testBrotchain() public {
+
+        uint[] memory brotchain_length = new uint[](100);
+        for (uint i = 0; i < 100; i++) {
+            updateImageByte();
+            string memory brotchain = b_renderer.bmpDataURI(b_renderer.bmp(r_img, 32, 32, palette));
+            brotchain_length[i] = bytes(brotchain).length;
+        }
+
+        emit log_uint(calc_mean(brotchain_length));
+        emit log_uint(calc_stddev(brotchain_length));
     }
 
     function testPixelations() public {
