b1c3a2e6f9835c8c5192261c0a169c6d5aade663
Peiyuan Liao <peiyuanl@cs.cmu.edu>
2022-03-16 20:44:53 -0400

    benchmark EBMP vs Pixelations

---
+++ src/test/benchmark.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.11;
+
+import "ds-test/test.sol";
+import "src/EBMP.sol";
+import "src/PixelationsRenderer.sol";
+
+contract ContractTest is DSTest {
+    uint8[] img;
+    PixelationsRenderer renderer;
+    bytes img_bytes;
+    function setUp() public {
+        img = new uint8[](3072);
+        img_bytes = new bytes(736);
+        renderer = new PixelationsRenderer();
+    }
+
+    function testEBMP() public {
+        string memory ebmp = EBMP.encode(img, 32, 32, 3);
+    }
+
+    function testPixelations() public {
+        string memory pixelation = renderer.tokenSVG(img_bytes);
+    }
+}
